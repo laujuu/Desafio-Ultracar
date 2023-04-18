91a8e1236cba11ec2a8763ffd2ce62020f57b93c
laujuu <laurafayec@gmail.com>
2023-04-18 03:22:13 -0300

    feat: api flask para login de funcionario e implementação no front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import LoginFuncionario from './pages/LoginFuncionario';
+import AreaDoFuncionario from './pages/AreaDoFuncionario'
+
 import './App.css';
 
 class App extends React.Component {
@@ -9,6 +12,9 @@ class App extends React.Component {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/login-funcionario" element={<LoginFuncionario />} />
+          <Route path="/area-do-funcionario" element={<AreaDoFuncionario />} />
+
         </Routes>
       </BrowserRouter>
     );
---
+++ client/src/pages/AreaDoFuncionario.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AreaDoFuncionario() {
+  return (
+    <div className="App">
+      <h1 className='logo'>ultracar</h1>
+      <h2>Área de Atendimento</h2>
+      <h3>Use seu scanner para escanear o qrcode do cliente</h3>
+      {/* Todo: forma do funcionario escanear o qr code */}
+    </div>
+  );
+}
+
+export default AreaDoFuncionario;
